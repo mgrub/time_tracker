6509443ec9f28fd2bd3026dcb937fcaacc5f42f6
Maximilian Gruber <m.gruber@mailbox.org>
2019-11-01 00:29:52 +0100

    Add report script for visualization

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Small system tray utility to allow you to monitor your time usage.
 
 Icons are taken from https://openmoji.org/.
 
-A small visualization script is to be developed.
+A small visualization script is provided.
---
+++ report_generator.py
@@ -0,0 +1,43 @@
+import os
+import numpy
+import pandas
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+import matplotlib.pyplot as plt
+
+log_file = os.path.join("log.csv")
+
+df = pandas.read_csv(log_file, parse_dates=[0], index_col=0)
+
+# convert to local timezone
+df = df.tz_localize("UTC").tz_convert("Europe/Berlin")
+
+# keep only last two weeks of records
+cond = (df.index >= df.index.max() - pandas.Timedelta(days=14))
+df = df[cond]
+
+# calculate duration
+df['duration'] = numpy.roll(df.index.to_series(keep_tz=True).diff(),-1)#.astype("timedelta64[h]")
+
+# clean non-numerical and "Done"-entries
+df = df.dropna()
+df = df.where(df['action'] != "Done")
+
+# make report
+df_grouped = df.groupby([df.index.date, "action"])
+report = df_grouped['duration'].sum()
+print(report)
+
+ru = report.unstack(0).fillna(pandas.Timedelta(0))
+
+# make a stacked bar plot
+bottom = numpy.zeros(len(ru.columns))
+for action_label, row in ru.iterrows():
+    hours = row.values.astype("double") / (3600 * 1e9)   # nano-seconds to hours
+    plt.bar(ru.columns, hours, label=action_label, bottom=bottom)
+    bottom = hours
+
+plt.title("Tracked Time in hours")
+plt.legend()
+plt.show()
