1bd19498a5c91687cd0f8f34917c5158288704c3
Maximilian Gruber <maximilian.gruber@ptb.de>
2019-11-04 09:06:54 +0100

    Fix error with launch-script

--- launch_tray_app.sh
+++ launch_tray_app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 kdialog --passivepopup "Time Tracker started."
+cd ~/git_repos/time_tracker/
 nohup ~/python_venv/py3_qt/bin/python ~/git_repos/time_tracker/time_tracker.py &
-kdialog --passivepopup $(jobs)
+kdialog --passivepopup $!
 #disown "%nohup"
